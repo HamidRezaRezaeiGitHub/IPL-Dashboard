4cf1865d77d193eb6d2e0cda2ada063aa4154f95
Hamidreza Rezaei <hamidreza74hrr@yahoo.com>
2023-08-26 01:11:57 -0400

    styling of team page completed + pie chart for win/loss

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -2,6 +2,8 @@ import { React, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
+import { PieChart } from "react-minimal-pie-chart";
+import "./TeamPage.scss";
 
 export const TeamPage = () => {
   // This line uses the useState module in a way that a variable named "team" gets created, initially out of an empty JSON objetc (only with
@@ -30,13 +32,34 @@ export const TeamPage = () => {
 
   return (
     <div className="TeamPage">
-      <h1>{team.teamName}</h1>
+      <div className="team-name-section">
+        <h1 className="team-name">{team.teamName}</h1>
+      </div>
+      <div className="win-loss-section">
+        Wins / Losses
+        <PieChart
+          data={[
+            {
+              title: "Losses",
+              value: team.totalMatches - team.totalWins,
+              color: "#a34d5d",
+            },
+            { title: "Wins", value: team.totalWins, color: "#4da375" },
+          ]}
+        />
+      </div>
 
-      <MatchDetailCard teamName={team.teamName} match={team.matchesList[0]} />
+      <div className="match-detail-section">
+        <h3>Latest Matches</h3>
+        <MatchDetailCard teamName={team.teamName} match={team.matchesList[0]} />
+      </div>
 
       {team.matchesList.slice(1).map((match) => (
         <MatchSmallCard teamName={team.teamName} match={match} />
       ))}
+      <div className="more-link">
+        <a href="#">More ></a>
+      </div>
     </div>
   );
 };
